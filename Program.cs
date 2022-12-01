e403d488d33ab92aeb2c78da1c8e60cfa63697f2
Assistant18 <119590503+Assistant18@users.noreply.github.com>
2022-12-01 13:58:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstValue,SecondValue;
+
+            Console.WriteLine("Введите первое число");
+            
+            FirstValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+
+            SecondValue = int.Parse(Console.ReadLine());
+
+            int result = FirstValue + SecondValue;
+
+            Console.WriteLine(("Сумма чисел ровна: ") + result);
+
+            
+        }
+    }
+}
